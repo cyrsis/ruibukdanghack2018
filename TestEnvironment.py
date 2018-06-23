a397e44f8b387e0ca760125d19b50adc8e847384
Victor <em3888@gmail.com>
2018-06-23 12:50:02 +0800

    Get the basic up and running with K-mean

--- TestEnvironment.py
+++ TestEnvironment.py
@@ -1,8 +1,24 @@
-#victor 
-#23/06/2018 
+#victor
+#23/06/2018
 #Created @ 2018-06-23 10:02
+from keras import metrics
+from sklearn.cluster import KMeans
+from keras.datasets import mnist
+
+import numpy as np
+np.random.seed(2)
 
 
-from keras import backend as K
-
-K.tensorflow_backend._get_available_gpus()
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x = np.concatenate((x_train, x_test))
+y = np.concatenate((y_train, y_test))
+x = x.reshape((x.shape[0], -1))
+x = np.divide(x, 255.)
+# 10 clusters
+n_clusters = len(np.unique(y))
+# Runs in parallel 4 CPUs
+kmeans = KMeans(n_clusters=n_clusters, n_init=20, n_jobs=4)
+# Train K-Means.
+y_pred_kmeans = kmeans.fit_predict(x)
+# Evaluate the K-Means clustering accuracy.
+metrics.acc(y, y_pred_kmeans)
